a539bcd65544cb11d4c087c0eea3a217e493f88c
Masen Furer <mf@0x26.net>
2021-08-29 10:30:53 -0700

    tox.ini: use python generation only
    simplify the repo, use only python codeplug generator

--- input/default/generate.py
+++ input/default/generate.py
@@ -16,15 +16,11 @@ CodeplugRecipe(
     source_seattledmr=True,
     source_default_k7abd=True,
     source_k7abd=[(cp_dir / "k7abd")],
-    source_repeaterbook_proximity=cp_dir / "prox.csv",
-    repeaterbook_states=["washington", "oregon"],
-    repeaterbook_name_format='{Callsign} {Nearest City} {Landmark}',
-    scanlists_json=cp_dir / "scanlists.json",
     exclude=cp_dir / "exclude.csv",
-    order=cp_dir / "order.csv",
-    replacements=cp_dir / "replacements.csv",
-    output_anytone=True,
-    output_dmrconfig=[(cp_dir / "example-d878uv.conf")],
-    output_farnsworth=[(cp_dir / "example-md-uv380.json")],
-    output_gb3gf=True
+    output_farnsworth=[
+        (cp_dir / "md380-uhf.json"),
+        (cp_dir / "md380-vhf.json"),
+        (cp_dir / "md390-uhf.json"),
+        (cp_dir / "md390-vhf.json"),
+    ],
 ).generate(output / cp_dir.name)
